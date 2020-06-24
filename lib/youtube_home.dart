4cd919f4f0c2eeeebdc7b18ae83b53e1446ec0ff
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-24 23:54:50 +0100

    added bottom navigation bar

--- lib/youtube_home.dart
+++ lib/youtube_home.dart
@@ -10,22 +10,35 @@ class _YoutubeHomeState extends State<YoutubeHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset('assets/images/youtube_logo.png'),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.grey),
+        title: Image.asset(
+          'assets/images/youtube_logo.png',
+          width: 98.0,
+          height: 22.0,
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.videocam),
-            onPressed: (){},
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){},
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.account_circle),
-            onPressed: (){},
+            onPressed: () {},
           )
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(title: Text('Home'), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text('Trending'), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text('Subscriptions'), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text('Inbox'), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text('Library'), icon: Icon(Icons.home)),
+      ]),
     );
   }
 }
