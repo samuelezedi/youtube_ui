0be07bdba1daa24f2bfb7406ba50f3cfb01f6df4
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-26 23:00:16 +0100

    added constructor in VideoList

--- lib/screens/video_list.dart
+++ lib/screens/video_list.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:youtubeclone/models/youtube_model.dart';
 
 class VideoList extends StatelessWidget {
+
+  final listData;
+
+  const VideoList({Key key, this.listData}) : super(key:key);
+
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
